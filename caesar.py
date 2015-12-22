1ecdb04ac77f5bb96f476f8f8cd84baa5bd4d009
Adnan EROL <adnanerol38@users.noreply.github.com>
2015-12-23 01:10:27 +0200

    Create caesar.py

---
+++ caesar.py
@@ -0,0 +1,17 @@
+import string
+
+#Kullanicidan sifrelenecek metinin alinmasi
+plainText = raw_input("What is your plaintext? ")
+
+#Kullanicidan sifrelenecek metinin kac harf ileri shift edileceginin alinmasi
+shift = int(raw_input("What is your shift? "))
+
+#Sifreleme fonksiyonu, verlen stringin karakterlerini 
+#shift miktarinca ilerleterek degistirir. 
+def caesar(plaintext, shift):
+	alphabet = string.ascii_lowercase
+	shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+	table = string.maketrans(alphabet, shifted_alphabet)
+	return plaintext.translate(table)
+
+print caesar(plainText, shift)
